e74384f9e6d8d08e78ccba4bb3adeb4ef068a820
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-08-28 09:02:06 +0200

    connect materializeCss

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
 
@@ -14,9 +15,11 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(todoRoutes)
+
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://alex:alexalex1@cluster0.l1m9d.mongodb.net/todos', {
+        await mongoose.connect('mongodb+srv://alex:alexalex1@cluster0.l1m9d.mongodb.net/mongo-node-todo-app', {
             useNewUrlParser: true,
             useFindAndModify: false,
             useUnifiedTopology: true
---
+++ routes/todos.js
@@ -0,0 +1,17 @@
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Todos List',
+        isIndex: true
+    })
+})
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create Todo',
+        isCreate: true
+    })
+})
+
+module.exports = router
